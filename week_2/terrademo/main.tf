fc9266f3f7e55efdc039dd2d1e1477b4c12ac205
Radu-Stroe <stroeradu03@gmail.com>
2025-01-24 09:43:33 +0200

    Update terrademo

--- week_2/terrademo/main.tf
+++ week_2/terrademo/main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "terraform-demo-448418"
-  region  = "europe-central2"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "terraform-demo-448418-terra-bucket"
-  location      = "EU"
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +26,9 @@ resource "google_storage_bucket" "auto-expire" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
